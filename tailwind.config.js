5a4507fd3c890b9e070aeafa5d6a6dc72352bb9c
Allen Gueco <allen.gueco1@gmail.com>
2024-09-16 20:25:40 -0400

    working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./projects/website/src/**/*.{html,ts}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
