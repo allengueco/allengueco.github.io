660e34a03ae38eccf013de7b79eda2d189f21d03
Allen Gueco <allen.gueco1@gmail.com>
2024-10-07 05:26:26 -0400

    experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -14,21 +14,23 @@ export class ExperienceComponent {
     this.experiences = [
       {
         title: 'Senior Associate, Full Stack Engineer',
-        company: 'BNY Mellon | Pershing',
+        company: 'BNY Mellon',
         duration: '2020 — 2024',
-        summary: "Built, maintained, and improved critical Spring Boot applications for the Pershing Architecture team. " +
-            "Designed Angular applications for both internal and client-facing tools. " +
-            "Worked in Agile-based development teams with members located across US and India. " +
-            "Containerized applications using Docker for deploying on-prem. ",
+        summary: "Built, maintained, and improved critical Spring Boot applications for the Architecture team. " +
+            "Designed Angular applications for both internal tools and client-facing components. " +
+            "Worked in Agile teams with members located across US and India. " +
+            "Containerized frontend and backend applications using Docker for deploying on-prem. ",
         skills: [
-          "Java", "Spring Boot", "Angular", "Docker", "CI/CD", "TypeScript"
+          "Java", "Spring Boot", "Angular", "Docker", "TypeScript"
         ]
       },
       {
         title: "EDI Intern",
-        company: "AmerisourceBergen Corporation (now Cencora)",
-        duration: '2020 — 2020',
-        summary: "",
+        company: "Cencora",
+        duration: "Summer \'19",
+        summary: "Developed an internal tool for the Electronic Data Interchange (EDI) team to ensure data integrity for b2b transactions. " +
+            "Designed APIs through MuleSoft to provide a RESTful service for the team. " +
+            "Created an Oracle PL/SQL procedure that emails a report based on an ad-hoc query.",
         skills: [
           "MuleSoft", "PL/SQL", "API"
         ]
