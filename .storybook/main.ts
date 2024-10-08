12ac3383fc98fc3f2bbcca646ad444b3c12108a3
Allen Gueco <allen.gueco1@gmail.com>
2024-10-08 02:02:29 -0400

    update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,19 +2,25 @@ import type { StorybookConfig } from "@storybook/angular";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
+
   framework: {
     name: "@storybook/angular",
     options: {},
   },
+
   core: {
     disableTelemetry: true,
-  }
+  },
+
+  docs: {}
 };
 export default config;
