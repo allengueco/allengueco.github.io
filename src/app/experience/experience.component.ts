80cabe4d5e3e6f93fa623ff15ccca82606262c7c
Allen Gueco <allen.gueco1@gmail.com>
2024-10-27 18:54:00 -0400

    ci maybe?

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -17,9 +17,9 @@ export class ExperienceComponent {
         company: 'BNY Mellon',
         duration: '2020 — 2024',
         summary: "Built, maintained, and improved critical Spring Boot applications for the Architecture team. " +
-            "Designed Angular applications for both internal tools and client-facing components. " +
-            "Worked in Agile teams with members located across US and India. " +
-            "Containerized frontend and backend applications using Docker for deploying on-prem. ",
+          "Designed Angular applications for both internal tools and client-facing components. " +
+          "Worked in Agile teams with members located across US and India. " +
+          "Containerized frontend and backend applications using Docker for deploying on-prem. ",
         skills: [
           "Java", "Spring Boot", "Angular", "Docker", "TypeScript"
         ]
@@ -29,8 +29,8 @@ export class ExperienceComponent {
         company: "Cencora",
         duration: "Summer \'19",
         summary: "Developed an internal tool for the Electronic Data Interchange (EDI) team to ensure data integrity for b2b transactions. " +
-            "Designed APIs through MuleSoft to provide a RESTful service for the team. " +
-            "Created an Oracle PL/SQL procedure that emails a report based on an ad-hoc query.",
+          "Designed APIs through MuleSoft to provide a RESTful service for the team. " +
+          "Created an Oracle PL/SQL procedure that emails a report based on an ad-hoc query.",
         skills: [
           "MuleSoft", "PL/SQL", "API"
         ]
